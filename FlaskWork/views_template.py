33d7ecdd2b9dd975f9477020d441de09890e033c
张仁山 <1041369205@qq.com>
2019-10-07 19:33:17 +0800

    第四次提交：Flask 静态文件

--- FlaskWork/views_template.py
+++ FlaskWork/views_template.py
@@ -11,6 +11,11 @@ def index():
     return render_template('index.html', **locals())
 
 
+@app.route('/login/')
+def login():
+    return render_template('login.html', **locals())
+
+
 # 注意点： 在初期，执行哪个文件下的视图函数和路由，就运行哪个文件
 if __name__ == '__main__':
     # host 地址       port 端口     debug 是否打开调试
