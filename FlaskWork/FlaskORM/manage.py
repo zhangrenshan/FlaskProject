8b527109097d2a75372f8c2fd0e1cd01e9fb101e
张仁山 <1041369205@qq.com>
2019-10-07 23:58:36 +0800

    第十二次提交：flask-script插件（解决不用再Terminal命令行启动程序的问题）

--- FlaskWork/FlaskORM/manage.py
+++ FlaskWork/FlaskORM/manage.py
@@ -1,10 +1,25 @@
 import sys
 from models import models
 from views import app
+from flask_script import Manager
 
-print(sys.argv)
-command = sys.argv[1]
-if command == 'migrate':
-    models.create_all()
-elif command == 'runserver':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+# print(sys.argv)
+# command = sys.argv[1]
+# if command == 'migrate':
+#     models.create_all()
+# elif command == 'runserver':
+#     app.run(host='127.0.0.1', port=8000, debug=True)
+
+manage = Manager(app)
+
+@manage.command
+def hello():
+    print('hello')
+
+
+# 如果想不用再Terminal命令行输入命令，直接在项目里运行
+# 需要在Edit Config中更改Target为manage.py
+
+
+if __name__ == '__main__':
+    manage.run()
