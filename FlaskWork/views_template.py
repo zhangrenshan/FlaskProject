558ee3c005963f24e63ffef60864037c42fa80e0
张仁山 <1041369205@qq.com>
2019-10-07 19:24:41 +0800

    第三次提交：渲染

---
+++ FlaskWork/views_template.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    name = 'Jack'
+    return render_template('index.html', **locals())
+
+
+# 注意点： 在初期，执行哪个文件下的视图函数和路由，就运行哪个文件
+if __name__ == '__main__':
+    # host 地址       port 端口     debug 是否打开调试
+    app.run(host='127.0.0.1', port=8000, debug=True)    # 启动这个应用
--- FlaskWork/views_url.py
+++ FlaskWork/views_url.py
@@ -19,6 +19,7 @@ def content(age, username):
     return "hello ,I am %s,I am %s years old"%(username, age)
 
 
+# methods参数默认GET,如果是POST，需在GET的原基础上添加（而不是删除GET）
 @app.route("/method/", methods=["GET", "POST"])
 def method():
     return "hello ,this is mentod"
